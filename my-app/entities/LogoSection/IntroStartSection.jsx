d3ec3488ce366fd9ea2cdcbf7f6c52d3ba88edc2
chaemina <chaemina82@naver.com>
2025-07-07 18:20:19 +0900

    fix

--- my-app/entities/LogoSection/IntroStartSection.jsx
+++ my-app/entities/LogoSection/IntroStartSection.jsx
@@ -19,6 +19,8 @@ const IntroStartSection = () => {
         web programming, graghic design, and Korean cultural classes for students at Hangai University
         in Mongolia. Our team, name “CoKoMong” stands for Korea and Mongolia connected through Coding.
         </p>
+        <br/>
+        <p> For any inquiries or suggestions, please contact the team leader via email or Instagram direct message. </p>
       </div>
     </div>
   );
