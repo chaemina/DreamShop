9d58ca23d04150c1e2cba701543fe406bd6e363b
chaemina <chaemina82@naver.com>
2025-08-01 21:25:32 +0900

    fix

--- my-app/shared/constants/ger.js
+++ my-app/shared/constants/ger.js
@@ -2,5 +2,5 @@ export const gers = [
   { id: 1, name: "Chae Min A", link: "https://my-ger-shop.vercel.app/", image: "/images/Ger1.png"},
   { id: 2, name: "Kim Hee Joeng", link: "https://hj-shop.vercel.app/", image: "/images/Ger2.png" },
   { id: 3, name: "Jeong Seon Hyung", link: "https://sh-shop-six.vercel.app/", image: "/images/Ger1.png" },
-  { id: 4, name: "Park Jae Yong", link: "/ger/1", image: "/images/Ger2.png" },
+  { id: 4, name: "Park Jae Yong", link: "https://jy-shop.vercel.app/", image: "/images/Ger2.png" },
 ];
