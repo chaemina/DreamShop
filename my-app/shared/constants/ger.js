92bab1cc5603716e1b3f58041be52c82a6655f30
chaemina <chaemina82@naver.com>
2025-08-14 22:28:36 +0900

    fix

--- my-app/shared/constants/ger.js
+++ my-app/shared/constants/ger.js
@@ -1,8 +1,8 @@
 export const gers = [
-  { id: 1, name: "Chae Min A", link: "https://my-ger-shop.vercel.app/", image: "/images/Ger1.png"},
-  { id: 2, name: "Kim Hee Joeng", link: "https://hj-shop.vercel.app/", image: "/images/Ger2.png" },
-  { id: 3, name: "Jeong Seon Hyung", link: "https://sh-shop-six.vercel.app/", image: "/images/Ger1.png" },
-  { id: 4, name: "Park Jae Yong", link: "https://jy-shop.vercel.app/", image: "/images/Ger2.png" },
+  { id: 1, name: "민아", link: "https://my-ger-shop.vercel.app/", image: "/images/Ger1.png"},
+  { id: 2, name: "희정", link: "https://hj-shop.vercel.app/", image: "/images/Ger2.png" },
+  { id: 3, name: "선형", link: "https://sh-shop-six.vercel.app/", image: "/images/Ger1.png" },
+  { id: 4, name: "재용", link: "https://jy-shop.vercel.app/", image: "/images/Ger2.png" },
   { id: 5, name: "은유", link: "https://haliuka.vercel.app/", image: "/images/Ger1.png"},
   { id: 6, name: "하나", link: "https://anarmaa.vercel.app/", image: "/images/Ger2.png" },
   { id: 7, name: "지성", link: "https://anand-six.vercel.app/", image: "/images/Ger1.png" },
